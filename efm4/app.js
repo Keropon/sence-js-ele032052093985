8832b3efc07b7d854c0c40d9d6e6645876a32682
Alexander Poyanco <daisuke.exe@gmail.com>
2025-12-22 22:57:18 -0300

    feat: select by ingredient

--- efm4/app.js
+++ efm4/app.js
@@ -1,7 +1,8 @@
-const API_URL = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
+const API_INGREDIENTS = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+const API_FILTER = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
 const FORM = document.querySelector('#search-form');
 const RESULTADOS = document.querySelector('#resultados');
-const INPUT = document.querySelector('#ingrediente');
+const SELECT = document.querySelector('#ingrediente');
 const TEMPLATE = document.querySelector('#receta-template');
 const MSG_NO_RESULTS = '<div class="col-12"><p class="text-center text-muted">Lo sentimos, no se encontraron recetas. Intenta con otro ingrediente.</p></div>';
 
@@ -16,11 +17,17 @@ class Receta {
   };
 }
 
+(async () => {
+  const { meals } = await (await fetch(API_INGREDIENTS)).json();
+  SELECT.innerHTML = '<option value="">Selecciona un ingrediente...</option>' + meals.map(({ strIngredient }) => `<option value="${strIngredient}">${strIngredient}</option>`).join('');
+})();
+
 FORM.addEventListener('submit', async e => {
   e.preventDefault();
-  const ingrediente = INPUT.value.trim();
+  const ingrediente = SELECT.value.trim();
+  if (!ingrediente) return;
   try {
-    const { meals } = await (await fetch(API_URL + ingrediente)).json();
+    const { meals } = await (await fetch(API_FILTER + ingrediente)).json();
     RESULTADOS.innerHTML = '';
     meals ? meals.forEach(meal => RESULTADOS.appendChild(new Receta(meal).toHTML())) : RESULTADOS.innerHTML = MSG_NO_RESULTS;
   } catch (err) { console.error('Error:', err); }
